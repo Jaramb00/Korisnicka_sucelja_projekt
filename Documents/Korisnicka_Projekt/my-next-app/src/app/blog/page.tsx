7b79239324417de79159601c22aa81ac432c2261
Jaramb00 <jaramb00@fesb.hr>
2024-12-12 22:03:08 +0100

    Dodana nova slika u [ime foldera]

--- Documents/Korisnicka_Projekt/my-next-app/src/app/blog/page.tsx
+++ Documents/Korisnicka_Projekt/my-next-app/src/app/blog/page.tsx
@@ -7,9 +7,9 @@ export const metadata: Metadata = {
 export default function BlogPage() {
   return (
     <Layout>
-    <main className="flex min-h-screen flex-col items-center p-10">
-      <h1 className="text-6xl font-extrabold tracking-tight">Blog</h1>
-    </main>
+      <main className="flex min-h-screen flex-col items-center p-10">
+        <h1 className="text-6xl font-extrabold tracking-tight">Blog</h1>
+      </main>
     </Layout>
   );
 }
--- Documents/Korisnicka_Projekt/my-next-app/src/app/components/layout.tsx
+++ Documents/Korisnicka_Projekt/my-next-app/src/app/components/layout.tsx
@@ -8,13 +8,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <header>
         <nav>
           <ul className="flex space-x-4 p-4 bg-gray-200">
-            <li><Link href="/">Home page</Link></li>
-            <li><Link href="/case_studies">Case studies</Link></li>
-            <li><Link href="/industries">Industries</Link></li>
-            <li><Link href="/services">Services</Link></li>
-            <li><Link href="/blog">Blog</Link></li>
-            <li><Link href="/about">About us</Link></li>
-            <li><Link href="/contact">Contact us</Link></li>
+            <li><Link href="/" className="text-1xl font-bold tracking-tight">Home page</Link></li>
+            <li><Link href="/case_studies" className="text-1xl font-bold tracking-tight">Case studies</Link></li>
+            <li><Link href="/industries" className="text-1xl font-bold tracking-tight">Industries</Link></li>
+            <li><Link href="/services" className="text-1xl font-bold tracking-tight">Services</Link></li>
+            <li><Link href="/blog" className="text-1xl font-bold tracking-tight">Blog</Link></li>
+            <li><Link href="/about" className="text-1xl font-bold tracking-tight">About us</Link></li>
+            <li><Link href="/contact" className="text-1xl font-bold tracking-tight">Contact us</Link></li>
           </ul>
         </nav>
       </header>
